7e691a80e1e1f8100fc2b8225bebcc0f4892dfe8
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2025-01-01 12:27:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ def get_video_info():
         return f"Error fetching data: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080,debug=True)
+
