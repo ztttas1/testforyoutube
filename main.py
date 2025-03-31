f4ed5bc9e4705f0d06dcb020dbb1fb8f59f680cf
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2025-03-31 10:12:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -125,6 +125,5 @@ def search_videos():
     except requests.exceptions.RequestException as e:
         return f"Error: {str(e)}", 500
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080,debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
