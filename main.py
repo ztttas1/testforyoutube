42e9eccf5240958d3a3eba764dcdededb5b3495d
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2025-04-01 17:06:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def search_videos():
                 </form>
                 <h2>検索結果</h2>
             """
-            for video in results[:f"{SEARCH}"]:
+            for video in results[:20]:
                 video_id = video.get('videoId')
                 title = video.get('title')
                 thumbnails = video.get('videoThumbnails')
