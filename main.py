134c291c6c7f779031b82036819b6cbd154c0ced
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2025-06-14 17:27:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def get_stream_url():
             response.raise_for_status()
 
             data = response.json()
-            if response in "highstreamUrl":
+            if "highstreamUrl" in response:
                 stream_url = data.get('highstreamUrl')
             else:
                 stream_url = data.get('stream_url')
@@ -257,7 +257,7 @@ def get_stream_url():
         response.raise_for_status()
 
         data = response.json()
-        if response in "highstreamUrl":
+        if "highstreamUrl" in response:
             stream_url = data.get('highstreamUrl')
         else:
             stream_url = data.get('stream_url')
